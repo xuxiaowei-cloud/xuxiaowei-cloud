b0cf928a7cb2a46bff6efc0c8b82e29b7fba9cda
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-02-21 22:29:37 +0800

    Configuration passport cloud

--- passport/src/main/java/cloud/xuxiaowei/passport/PassportApplication.java
+++ passport/src/main/java/cloud/xuxiaowei/passport/PassportApplication.java
@@ -3,6 +3,12 @@ package cloud.xuxiaowei.passport;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 程序启动入口
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
 @SpringBootApplication
 public class PassportApplication {
 
