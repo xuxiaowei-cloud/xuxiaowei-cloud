c3d53d0d319b7d92987393df86e813367038098b
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-02-22 20:17:27 +0800

    新增网关

---
+++ gateway/src/main/java/cloud/xuxiaowei/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package cloud.xuxiaowei.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 程序启动入口
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
