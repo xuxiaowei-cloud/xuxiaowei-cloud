97b4dcd819d6848c90d4b9e45da821ecf7582364
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-02-23 22:06:10 +0800

    管理服务

---
+++ admin-server/src/main/java/cloud/xuxiaowei/admin/server/AdminServerApplication.java
@@ -0,0 +1,21 @@
+package cloud.xuxiaowei.admin.server;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 程序启动入口
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@EnableAdminServer
+@SpringBootApplication
+public class AdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class, args);
+    }
+
+}
