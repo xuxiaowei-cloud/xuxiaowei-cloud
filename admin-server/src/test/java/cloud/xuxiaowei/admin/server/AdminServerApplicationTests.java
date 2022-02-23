534db8fbc1dcd3ed45d2297a658eb619d30ad9cf
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-02-23 22:06:10 +0800

    管理服务

---
+++ admin-server/src/test/java/cloud/xuxiaowei/admin/server/AdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.xuxiaowei.admin.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
