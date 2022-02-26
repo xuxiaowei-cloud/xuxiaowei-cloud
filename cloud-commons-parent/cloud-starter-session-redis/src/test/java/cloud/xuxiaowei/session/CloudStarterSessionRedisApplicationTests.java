120fedd3c6e0c50e4cd8a4117893a1d89ebc3240
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-02-26 16:20:55 +0800

    新增：Session Redis 组件

---
+++ cloud-commons-parent/cloud-starter-session-redis/src/test/java/cloud/xuxiaowei/session/CloudStarterSessionRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.xuxiaowei.session;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStarterSessionRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
