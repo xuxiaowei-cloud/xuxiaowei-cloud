470861ffe52379485e4fe6e7606d9c383df58768
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-02-26 15:50:27 +0800

    Redis组件

---
+++ cloud-commons-parent/cloud-starter-redis/src/main/java/cloud/xuxiaowei/redis/CloudStarterRedisApplication.java
@@ -0,0 +1,12 @@
+package cloud.xuxiaowei.redis;
+
+/**
+ * 暂无意义，仅为了打包不报错
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+public class CloudStarterRedisApplication {
+
+
+}
---
+++ cloud-commons-parent/cloud-starter-redis/src/test/java/cloud/xuxiaowei/redis/CloudStarterRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.xuxiaowei.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStarterRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
