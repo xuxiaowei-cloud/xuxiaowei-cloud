634e6820557702d42184df5fa9aba46e2878de76
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-02-26 19:44:35 +0800

    工具类

---
+++ utils/src/main/java/cloud/xuxiaowei/utils/DateUtils.java
@@ -0,0 +1,26 @@
+package cloud.xuxiaowei.utils;
+
+/**
+ * 日期工具类
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+public class DateUtils {
+
+    /**
+     * 默认时间格式
+     */
+    public static final String DEFAULT_TIME_FORMAT = "HH:mm:ss";
+
+    /**
+     * 默认日期格式
+     */
+    public static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd";
+
+    /**
+     * 默认日期时间格式
+     */
+    public static final String DEFAULT_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+}
