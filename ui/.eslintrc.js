4332da2dd5b87800d55955056ea95e533656f8dc
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-03-01 00:07:06 +0800

    处理：'defineProps' is not defined  no-undef

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    // 'defineProps' is not defined  no-undef
+    'vue/setup-compiler-macros': true
   },
   extends: [
     'plugin:vue/vue3-essential',
