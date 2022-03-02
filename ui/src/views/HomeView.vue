5f120faa49a183d3d8cccbad340a6946ac296e84
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-03-02 23:14:07 +0800

    add element-plus

--- ui/src/views/HomeView.vue
+++ ui/src/views/HomeView.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
     Home
+    <br>
+    <el-button>I am ElButton</el-button>
   </div>
 </template>
 
 <script>
+import { ElButton } from 'element-plus'
 export default {
-  name: 'HomeView'
+  name: 'HomeView',
+  components: { ElButton },
 }
 </script>
 
