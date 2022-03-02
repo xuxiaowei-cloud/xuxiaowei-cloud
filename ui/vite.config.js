5088639f3e13a04df4a0cf3f16634fa886ac94f8
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-03-02 23:08:12 +0800

    add axios

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -3,5 +3,20 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    // 代理
+    proxy: {
+      '/actuator': {
+        target: 'http://127.0.0.1:1101'
+      },
+      '/baidu': {
+        target: 'https://www.baidu.com',
+        // 将主机标头的来源更改为目标 URL。
+        changeOrigin: true,
+        // 重写地址
+        rewrite: (path) => path.replace(/^\/baidu/, '')
+      }
+    }
+  }
 })
