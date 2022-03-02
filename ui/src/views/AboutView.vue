45e11d31e0e51421030cf214e6389d9388ee9544
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-03-02 23:08:12 +0800

    add axios

--- ui/src/views/AboutView.vue
+++ ui/src/views/AboutView.vue
@@ -5,8 +5,21 @@
 </template>
 
 <script>
+import { actuator, baidu } from '../api/actuator'
+
 export default {
-  name: 'AboutView'
+  name: 'AboutView',
+  mounted() {
+    this.$ipv4().then(response => {
+      console.log(response)
+    })
+    actuator().then(response => {
+      console.log(response)
+    })
+    baidu().then(response => {
+      console.log(response)
+    })
+  }
 }
 </script>
 
