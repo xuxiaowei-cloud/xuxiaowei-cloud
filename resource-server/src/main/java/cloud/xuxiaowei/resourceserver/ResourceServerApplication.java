b559f3f04946013557ed2fcb570bd55a72c1ed4b
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-03-03 22:21:42 +0800

    init resource-server

---
+++ resource-server/src/main/java/cloud/xuxiaowei/resourceserver/ResourceServerApplication.java
@@ -0,0 +1,19 @@
+package cloud.xuxiaowei.resourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 程序执行入口
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@SpringBootApplication
+public class ResourceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceServerApplication.class, args);
+    }
+
+}
