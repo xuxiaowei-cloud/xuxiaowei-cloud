b9ad46b435c9d49c8ca0eb6c273c159db5cfc8d9
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-03-03 22:21:42 +0800

    init resource-server

---
+++ resource-server/src/test/java/cloud/xuxiaowei/resourceserver/ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.xuxiaowei.resourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
