c295c9beb2375722c020645e5476d5ea7a45127c
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-03-07 21:08:31 +0800

    add 核心组件

---
+++ cloud-commons-parent/cloud-starter-core/src/main/java/cloud/xuxiaowei/core/CloudStarterCoreApplication.java
@@ -0,0 +1,17 @@
+package cloud.xuxiaowei.core;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * 暂无意义，仅为了打包不报错
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+public class CloudStarterCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudStarterCoreApplication.class, args);
+    }
+
+}
