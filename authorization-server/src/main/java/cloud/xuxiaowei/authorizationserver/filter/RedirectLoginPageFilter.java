5a54d2880337c2622c695ab78e9a41362be1ef3a
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-03-08 15:57:58 +0800

    公共属性读取移动到核心模块

--- authorization-server/src/main/java/cloud/xuxiaowei/authorizationserver/filter/RedirectLoginPageFilter.java
+++ authorization-server/src/main/java/cloud/xuxiaowei/authorizationserver/filter/RedirectLoginPageFilter.java
@@ -1,6 +1,6 @@
 package cloud.xuxiaowei.authorizationserver.filter;
 
-import cloud.xuxiaowei.authorizationserver.properties.CloudProperties;
+import cloud.xuxiaowei.core.properties.CloudProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.authentication.ui.DefaultLoginPageGeneratingFilter;
