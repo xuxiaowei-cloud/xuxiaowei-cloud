2fffd5b82ce6192d064791b7a9acc6493b3261f0
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-03-08 19:02:03 +0800

    使用 createWebHistory

--- passport-ui/src/router/index.js
+++ passport-ui/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes = [
@@ -19,7 +19,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
