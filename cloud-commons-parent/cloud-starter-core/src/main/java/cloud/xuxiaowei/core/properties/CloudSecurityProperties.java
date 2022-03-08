ffbd306228dc8013678bfb4896cf449ea8601813
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-03-08 17:15:04 +0800

    rename

--- cloud-commons-parent/cloud-starter-core/src/main/java/cloud/xuxiaowei/core/properties/CloudSecurityProperties.java
+++ cloud-commons-parent/cloud-starter-core/src/main/java/cloud/xuxiaowei/core/properties/CloudSecurityProperties.java
@@ -5,15 +5,15 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 /**
- * 微服务 配置
+ * 微服务 Security 配置
  *
  * @author xuxiaowei
  * @since 0.0.1
  */
 @Data
 @Component
-@ConfigurationProperties("cloud")
-public class CloudProperties {
+@ConfigurationProperties("cloud.security")
+public class CloudSecurityProperties {
 
     /**
      * 登录页面地址
