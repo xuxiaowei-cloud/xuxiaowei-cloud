8a1123f5cb9994265b9fa1188af4b572989fb4b8
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-03-14 20:45:09 +0800

    fix 登录异常

--- utils/src/main/java/cloud/xuxiaowei/utils/exception/LoginParamPasswordValidException.java
+++ utils/src/main/java/cloud/xuxiaowei/utils/exception/LoginParamPasswordValidException.java
@@ -14,8 +14,36 @@ public class LoginParamPasswordValidException extends LoginParamValidException {
         super(CodeEnums.A1202.code, CodeEnums.A1202.msg);
     }
 
+    public LoginParamPasswordValidException(String msg) {
+        super(CodeEnums.A1202.code, msg);
+    }
+
     public LoginParamPasswordValidException(String code, String msg) {
         super(code, msg);
+    }
+
+    public LoginParamPasswordValidException(String code, String msg, Throwable cause) {
+        super(code, msg, cause);
+    }
+
+    public LoginParamPasswordValidException(String code, String msg, String field, Throwable cause) {
+        super(code, msg, field, cause);
+    }
+
+    public LoginParamPasswordValidException(String code, String msg, String field, String explain, Throwable cause) {
+        super(code, msg, field, explain, cause);
+    }
+
+    public LoginParamPasswordValidException(Throwable cause) {
+        super(CodeEnums.A1202.msg, cause);
+        this.code = CodeEnums.A1202.code;
+        this.msg = CodeEnums.A1202.msg;
+    }
+
+    public LoginParamPasswordValidException(String msg, Throwable cause) {
+        super(msg, cause);
+        this.code = CodeEnums.A1202.code;
+        this.msg = msg;
     }
 
 }
--- utils/src/main/java/cloud/xuxiaowei/utils/exception/LoginParamValidException.java
+++ utils/src/main/java/cloud/xuxiaowei/utils/exception/LoginParamValidException.java
@@ -14,8 +14,36 @@ public class LoginParamValidException extends LoginException {
         super(CodeEnums.A1200.code, CodeEnums.A1200.msg);
     }
 
+    public LoginParamValidException(String msg) {
+        super(CodeEnums.A1200.code, msg);
+    }
+
     public LoginParamValidException(String code, String msg) {
         super(code, msg);
+    }
+
+    public LoginParamValidException(String code, String msg, Throwable cause) {
+        super(code, msg, cause);
+    }
+
+    public LoginParamValidException(String code, String msg, String field, Throwable cause) {
+        super(code, msg, field, cause);
+    }
+
+    public LoginParamValidException(String code, String msg, String field, String explain, Throwable cause) {
+        super(code, msg, field, explain, cause);
+    }
+
+    public LoginParamValidException(Throwable cause) {
+        super(CodeEnums.A1200.msg, cause);
+        this.code = CodeEnums.A1200.code;
+        this.msg = CodeEnums.A1200.msg;
+    }
+
+    public LoginParamValidException(String msg, Throwable cause) {
+        super(msg, cause);
+        this.code = CodeEnums.A1200.code;
+        this.msg = msg;
     }
 
 }
