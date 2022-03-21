51bedbc8266d806aa2ac68f8ba04b8a03b6fb791
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-03-21 18:54:04 +0800

    add 别名：@

--- passport-ui/src/router/index.js
+++ passport-ui/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
 
 const routes = [
   {
--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
 
 const routes = [
   {
