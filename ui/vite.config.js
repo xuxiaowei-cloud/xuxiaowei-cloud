1e0803e3a9cfa363c4703f8bd55073dad93025b7
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-03-21 18:54:04 +0800

    add 别名：@

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +17,11 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()]
     })
   ],
+  resolve: {
+    alias: {
+      '@': resolve('./src')
+    }
+  },
   server: {
     // 代理
     proxy: {
