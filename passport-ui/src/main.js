570cf81931701aaae723be0a8a1a35517537bcff
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-03-23 00:00:19 +0800

    fix ElMessage 样式无效

--- passport-ui/src/main.js
+++ passport-ui/src/main.js
@@ -3,6 +3,8 @@ import App from '@/App.vue'
 import router from '@/router'
 import { ipv4, ipv6 } from '@/api/ipify'
 import request from '@/utils/request'
+// 防止 ElMessage 无效
+import 'element-plus/theme-chalk/el-message.css'
 
 const app = createApp(App)
 
