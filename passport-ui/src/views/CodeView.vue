ed8eaa057170a3aba73c90944fc6af37da816fd0
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-03-23 19:30:10 +0800

    fix 登录页面，防止刷新页面导致授权码 code 重复使用

--- passport-ui/src/views/CodeView.vue
+++ passport-ui/src/views/CodeView.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script setup>
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import { authorizationCode } from '@/api/code'
 
 const route = useRoute()
@@ -15,9 +15,21 @@ const state = route.query.code
 console.log('code', code)
 console.log('state', state)
 
-authorizationCode(code, state).then(response => {
-  console.log(response)
-})
+// 清空参数
+useRouter().push({ query: {} })
+
+if (!code && !state) {
+  console.log('code 与 state 都不存在')
+} else if (code && !state) {
+  console.log('code 存在，state 不存在')
+} else if (!code && state) {
+  console.log('code 不存在，state 存在')
+} else {
+  // 参数存在时，获取 Token
+  authorizationCode(code, state).then(response => {
+    console.log(response)
+  })
+}
 
 </script>
 
