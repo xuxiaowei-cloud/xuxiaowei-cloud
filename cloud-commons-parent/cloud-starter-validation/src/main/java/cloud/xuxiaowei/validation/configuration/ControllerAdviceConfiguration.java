0d75f6807f2c6ba1a9a4facc1e9ec3bfdded231f
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-03-24 22:54:47 +0800

    add 方法参数无效异常 统一处理

---
+++ cloud-commons-parent/cloud-starter-validation/src/main/java/cloud/xuxiaowei/validation/configuration/ControllerAdviceConfiguration.java
@@ -0,0 +1,77 @@
+package cloud.xuxiaowei.validation.configuration;
+
+import cloud.xuxiaowei.utils.Response;
+import com.google.common.base.Joiner;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * {@link Controller}、{@link RestController} 异常处理
+ * <p>
+ * 由于 {@link ControllerAdvice} 不支持通配符，故本配置直接扫描所有 {@link Controller}、{@link RestController} 父包
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@Slf4j
+@ControllerAdvice(basePackages = "cloud.xuxiaowei")
+public class ControllerAdviceConfiguration {
+
+    /**
+     * 请求时数据转换失败处理
+     *
+     * @param exception 异常
+     * @param request   请求
+     * @return 返回 验证结果
+     */
+    @ResponseBody
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Response<?> methodArgumentNotValidException(MethodArgumentNotValidException exception, WebRequest request) {
+
+        Response<?> error = Response.error("方法参数无效异常");
+
+        BindingResult bindingResult = exception.getBindingResult();
+        List<FieldError> fieldErrorList = bindingResult.getFieldErrors();
+
+        fieldErrorList(error, fieldErrorList);
+
+        log.error("方法参数无效异常：{}，异常：{}", error, exception);
+
+        return error;
+    }
+
+    /**
+     * 字段转换异常统一处理
+     *
+     * @param error          异常
+     * @param fieldErrorList 异常字段列表
+     */
+    private void fieldErrorList(Response<?> error, List<FieldError> fieldErrorList) {
+
+        List<String> fieldList = new ArrayList<>();
+        List<String> messageList = new ArrayList<>();
+
+        for (FieldError fieldError : fieldErrorList) {
+            String field = fieldError.getField();
+            fieldList.add(field);
+
+            String defaultMessage = fieldError.getDefaultMessage();
+            messageList.add(defaultMessage);
+        }
+
+        error.setField(Joiner.on(",").join(fieldList));
+        error.setMsg(Joiner.on(",").join(messageList));
+    }
+
+}
