2be74442e508fe07547766592cfea68fc2d049d4
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-03-24 19:19:18 +0800

    add 验证组件

---
+++ cloud-commons-parent/cloud-starter-validation/src/main/java/cloud/xuxiaowei/validation/CloudStarterValidationApplication.java
@@ -0,0 +1,18 @@
+package cloud.xuxiaowei.validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 暂无意义，仅为了打包不报错
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+public class CloudStarterValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudStarterValidationApplication.class, args);
+    }
+
+}
