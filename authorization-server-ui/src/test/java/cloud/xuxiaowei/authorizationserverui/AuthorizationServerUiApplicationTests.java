dabd07e5a18f176e095105beb81abd7e98b9c405
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-03-25 18:21:26 +0800

    add 使用 -Pexec 激活此构建打包可执行 jar 包

---
+++ authorization-server-ui/src/test/java/cloud/xuxiaowei/authorizationserverui/AuthorizationServerUiApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.xuxiaowei.authorizationserverui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationServerUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
