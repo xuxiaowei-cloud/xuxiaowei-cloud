fc2dd42d7e3e77eef157a7bfe4ed8d91928fcc37
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-03-25 18:21:26 +0800

    add 使用 -Pexec 激活此构建打包可执行 jar 包

---
+++ ui/src/test/java/cloud/xuxiaowei/ui/UiApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.xuxiaowei.ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
