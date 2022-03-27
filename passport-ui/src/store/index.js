e9da445adc96f94dda315e4e2a899bfd0c000810
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-03-27 16:34:52 +0800

    Token 放在 Cookie 中

--- passport-ui/src/store/index.js
+++ passport-ui/src/store/index.js
@@ -1,5 +1,6 @@
 import { createStore } from 'vuex'
 import createPersistedState from 'vuex-persistedstate'
+import Cookies from 'js-cookie'
 
 export default createStore({
   state: { // 单一状态树
@@ -23,6 +24,18 @@ export default createStore({
      */
     setToken (state, token) {
       state.token = token // 设置 Token
+
+      const expiration = token.expiration
+
+      const domain = '.xuxiaowei.cloud'
+      const expires = new Date(expiration)
+
+      Cookies.set('access_token', token.access_token, { path: '/', domain: domain, expires: expires })
+      Cookies.set('expiration', expiration, { path: '/', domain: domain, expires: expires })
+      Cookies.set('jti', token.jti, { path: '/', domain: domain, expires: expires })
+      Cookies.set('refresh_token', token.refresh_token, { path: '/', domain: domain, expires: expires })
+      Cookies.set('scope', token.scope, { path: '/', domain: domain, expires: expires })
+      Cookies.set('token_type', token.token_type, { path: '/', domain: domain, expires: expires })
     }
   },
   actions: {
