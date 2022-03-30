ac29596abd22a67b33e4e679905bd4374dbd7808
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-03-30 18:55:20 +0800

    add 监控（管理）服务 过滤器

---
+++ gateway/src/main/java/cloud/xuxiaowei/gateway/filter/AdminServerGlobalFilter.java
@@ -0,0 +1,54 @@
+package cloud.xuxiaowei.gateway.filter;
+
+import cloud.xuxiaowei.utils.Response;
+import cloud.xuxiaowei.utils.ResponseUtils;
+import cloud.xuxiaowei.utils.ServiceConstant;
+import lombok.Setter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+
+/**
+ * 监控（管理）服务 过滤器
+ * <p>
+ * 禁止通过网关访问 监控（管理）服务
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@Component
+public class AdminServerGlobalFilter implements GlobalFilter, Ordered {
+
+    public static final int ORDERED = Ordered.HIGHEST_PRECEDENCE + 20000;
+
+    @Setter
+    private int order = ORDERED;
+
+    @Override
+    public int getOrder() {
+        return order;
+    }
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        ServerHttpRequest request = exchange.getRequest();
+        URI uri = request.getURI();
+        String path = uri.getPath();
+        if (path.contains(ServiceConstant.ADMIN_SERVER)) {
+            ServerHttpResponse response = exchange.getResponse();
+            Response<?> error = Response.error("禁止通过网关访问 监控（管理）服务");
+            return ResponseUtils.writeWith(response, error);
+        }
+
+        return chain.filter(exchange);
+    }
+
+}
