03fca29b09057790c9757db5ad3749cd0ccf296c
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-03-31 21:22:22 +0800

    系统组件

---
+++ cloud-commons-parent/cloud-starter-system/src/main/java/cloud/xuxiaowei/system/CloudStarterSystemApplication.java
@@ -0,0 +1,11 @@
+package cloud.xuxiaowei.system;
+
+import org.springframework.boot.SpringApplication;
+
+public class CloudStarterSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudStarterSystemApplication.class, args);
+    }
+
+}
