1fc4adebdfe318d4394c68d5ce6bd214d18373df
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-04-01 13:18:21 +0800

    根据请求保存数据

--- gateway/src/main/java/cloud/xuxiaowei/gateway/filter/LogGlobalFilter.java
+++ gateway/src/main/java/cloud/xuxiaowei/gateway/filter/LogGlobalFilter.java
@@ -1,11 +1,12 @@
 package cloud.xuxiaowei.gateway.filter;
 
-import cloud.xuxiaowei.utils.CodeEnums;
-import cloud.xuxiaowei.utils.Response;
-import cloud.xuxiaowei.utils.ResponseUtils;
+import cloud.xuxiaowei.log.entity.Log;
+import cloud.xuxiaowei.log.service.ILogService;
+import cloud.xuxiaowei.utils.*;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
@@ -17,6 +18,9 @@ import reactor.core.publisher.Mono;
 
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.URI;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 import static cloud.xuxiaowei.utils.Constant.IP;
 
@@ -36,6 +40,13 @@ public class LogGlobalFilter implements GlobalFilter, Ordered {
      * 大于 0 无效
      */
     public static final int ORDERED = Ordered.HIGHEST_PRECEDENCE;
+
+    private ILogService logService;
+
+    @Autowired
+    public void setLogService(ILogService logService) {
+        this.logService = logService;
+    }
 
     @Setter
     private int order = ORDERED;
@@ -62,10 +73,59 @@ public class LogGlobalFilter implements GlobalFilter, Ordered {
         }
 
         InetAddress address = remoteAddress.getAddress();
-        String hostAddress = address.getHostAddress();
-        MDC.put(IP, hostAddress);
+        String remoteHost = address.getHostAddress();
+        MDC.put(IP, remoteHost);
+
+        save(logService, request, remoteHost);
 
         return chain.filter(exchange);
+    }
+
+    /**
+     * 根据请求保存数据
+     *
+     * @param logService 日志服务
+     * @param request    请求
+     * @param remoteHost 用户IP
+     */
+    public static void save(ILogService logService, ServerHttpRequest request, String remoteHost) {
+
+        LocalDate localDate = LocalDate.now();
+        int year = localDate.getYear();
+        int month = localDate.getMonthValue();
+        int day = localDate.getDayOfMonth();
+
+        LocalTime localTime = LocalTime.now();
+        int hour = localTime.getHour();
+
+        String method = request.getMethodValue();
+        String requestId = request.getId();
+
+        String headersMap = RequestUtils.getHeadersJson(request);
+        String userAgent = RequestUtils.getUserAgent(request);
+
+        URI uri = request.getURI();
+        String requestUri = uri.getPath();
+        String queryString = uri.getQuery();
+
+        Log log = new Log();
+        log.setModule(ServiceConstant.GATEWAY);
+        log.setDate(localDate);
+        log.setYear(year);
+        log.setMonth(month);
+        log.setDay(day);
+        log.setHour(hour);
+        log.setMethod(method);
+        log.setRequestUri(requestUri);
+        log.setQueryString(queryString);
+        log.setHeadersMap(headersMap);
+        log.setUserAgent(userAgent);
+        log.setRequestId(requestId);
+        log.setSessionId(null);
+        log.setCreateUsername("该字段待确认");
+        log.setCreateIp(remoteHost);
+
+        logService.save(log);
     }
 
 }
