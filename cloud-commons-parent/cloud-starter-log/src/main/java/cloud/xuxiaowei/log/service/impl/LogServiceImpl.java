b3b50fcf3a2586862e2e7b3677181a5c1f013f8c
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-04-01 14:12:54 +0800

    多数据源

--- cloud-commons-parent/cloud-starter-log/src/main/java/cloud/xuxiaowei/log/service/impl/LogServiceImpl.java
+++ cloud-commons-parent/cloud-starter-log/src/main/java/cloud/xuxiaowei/log/service/impl/LogServiceImpl.java
@@ -3,6 +3,7 @@ package cloud.xuxiaowei.log.service.impl;
 import cloud.xuxiaowei.log.entity.Log;
 import cloud.xuxiaowei.log.mapper.LogMapper;
 import cloud.xuxiaowei.log.service.ILogService;
+import com.baomidou.dynamic.datasource.annotation.DS;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,7 @@ import org.springframework.stereotype.Service;
  * @since 2022-04-01
  */
 @Service
+@DS("xuxiaowei_cloud_log")
 public class LogServiceImpl extends ServiceImpl<LogMapper, Log> implements ILogService {
 
 }
