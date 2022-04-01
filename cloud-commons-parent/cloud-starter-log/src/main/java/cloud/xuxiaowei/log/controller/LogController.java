375e9f50723c877eb8d59199075669fe4d042201
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-04-01 12:35:11 +0800

    add 日志表

---
+++ cloud-commons-parent/cloud-starter-log/src/main/java/cloud/xuxiaowei/log/controller/LogController.java
@@ -0,0 +1,18 @@
+package cloud.xuxiaowei.log.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 日志表 前端控制器
+ * </p>
+ *
+ * @author xuxiaowei
+ * @since 2022-04-01
+ */
+@RestController
+@RequestMapping("//log")
+public class LogController {
+
+}
---
+++ cloud-commons-parent/cloud-starter-log/src/main/java/cloud/xuxiaowei/log/service/impl/LogServiceImpl.java
@@ -0,0 +1,20 @@
+package cloud.xuxiaowei.log.service.impl;
+
+import cloud.xuxiaowei.log.entity.Log;
+import cloud.xuxiaowei.log.mapper.LogMapper;
+import cloud.xuxiaowei.log.service.ILogService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 日志表 服务实现类
+ * </p>
+ *
+ * @author xuxiaowei
+ * @since 2022-04-01
+ */
+@Service
+public class LogServiceImpl extends ServiceImpl<LogMapper, Log> implements ILogService {
+
+}
