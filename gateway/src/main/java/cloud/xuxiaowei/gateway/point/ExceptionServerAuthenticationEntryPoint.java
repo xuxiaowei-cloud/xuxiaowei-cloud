d65f82bd0a84b4feb854c0bc31bf6810e1843c67
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-04-03 15:44:27 +0800

    优化，代码抽取

--- gateway/src/main/java/cloud/xuxiaowei/gateway/point/ExceptionServerAuthenticationEntryPoint.java
+++ gateway/src/main/java/cloud/xuxiaowei/gateway/point/ExceptionServerAuthenticationEntryPoint.java
@@ -1,17 +1,12 @@
 package cloud.xuxiaowei.gateway.point;
 
-import cloud.xuxiaowei.gateway.filter.LogGlobalFilter;
+import cloud.xuxiaowei.gateway.handler.GatewayErrorWebExceptionHandler;
 import cloud.xuxiaowei.log.service.ILogService;
 import cloud.xuxiaowei.utils.CodeEnums;
-import cloud.xuxiaowei.utils.RequestUtils;
 import cloud.xuxiaowei.utils.Response;
 import cloud.xuxiaowei.utils.ResponseUtils;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
@@ -21,12 +16,6 @@ import org.springframework.security.web.server.ServerAuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
-
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-
-import static cloud.xuxiaowei.utils.Constant.IP;
-import static cloud.xuxiaowei.utils.Constant.REQUEST_ID;
 
 /**
  * 异常服务器认证入口点
@@ -51,45 +40,13 @@ public class ExceptionServerAuthenticationEntryPoint implements ServerAuthentica
 
         ServerHttpRequest request = exchange.getRequest();
         ServerHttpResponse response = exchange.getResponse();
-        HttpHeaders headers = response.getHeaders();
 
-        // 解决服务未发现时跨域问题
-        // Access to XMLHttpRequest at '……' from origin '……' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
-        headers.setAccessControlAllowOrigin(RequestUtils.getOrigin(request));
-        // Access to XMLHttpRequest at '……' from origin '……' has been blocked by CORS policy: The value of the 'Access-Control-Allow-Credentials' header in the response is '' which must be 'true' when the request's credentials mode is 'include'. The credentials mode of requests initiated by the XMLHttpRequest is controlled by the withCredentials attribute.
-        headers.setAccessControlAllowCredentials(true);
-
-        // 日志中放入请求ID
         String requestId = request.getId();
-        MDC.put(REQUEST_ID, requestId);
 
-        // 请求中放入用户IP
-        InetSocketAddress remoteAddress = request.getRemoteAddress();
-        if (remoteAddress == null) {
-            Response<?> error = Response.error(CodeEnums.X10003.code, CodeEnums.X10003.msg);
-            return ResponseUtils.writeWith(response, error);
+        Mono<Void> log = GatewayErrorWebExceptionHandler.log(logService, request, response, ex, requestId);
+        if (log != null) {
+            return log;
         }
-        InetAddress address = remoteAddress.getAddress();
-        String hostName = address.getHostName();
-        String hostAddress = address.getHostAddress();
-        MDC.put(IP, hostAddress);
-
-        LogGlobalFilter.save(logService, request, hostAddress, hostName, ex);
-
-        MediaType contentType = request.getHeaders().getContentType();
-
-        if (contentType == null) {
-            headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
-        } else if (contentType.equals(MediaType.APPLICATION_JSON)) {
-            headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
-        } else {
-            headers.setContentType(contentType);
-        }
-
-        headers.setAccept(request.getHeaders().getAccept());
-        response.setStatusCode(HttpStatus.OK);
-
-        log.error(requestId, ex);
 
         Response<?> error = Response.error(CodeEnums.T10001.code, CodeEnums.T10001.msg);
         error.setRequestId(requestId);
