7ec9cdddd804adfb337aebe713b6524e8ae3bd0e
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-04-05 23:31:15 +0800

    新增字段

--- cloud-commons-parent/cloud-starter-system/src/main/java/cloud/xuxiaowei/system/entity/Users.java
+++ cloud-commons-parent/cloud-starter-system/src/main/java/cloud/xuxiaowei/system/entity/Users.java
@@ -9,11 +9,11 @@ import java.util.List;
 
 /**
  * <p>
- * 用户表。	原表结构：spring-security-core-*.*.*.jar!/org/springframework/security/core/userdetails/jdbc/users.ddl	原表结构：https://github.com/spring-projects/spring-security/blob/main/core/src/main/resources/org/springframework/security/core/userdetails/jdbc/users.ddl	GitCode 镜像仓库：https://gitcode.net/mirrors/spring-projects/spring-security/blob/main/core/src/main/resources/org/springframework/security/core/userdetails/jdbc/users.ddl
+ * 用户表。	原表结构：spring-security-core-*.*.*.jar!/org/springframework/security/core/userdetails/jdbc/users.ddl	原表结构：https://github.com/spring-projects/spring-security/blob/main/core/src/main/resources/org/springframework/security/core/userdetails/jdbc/users.ddl	GitCode 镜像仓库：https://gitcode.net/mirrors/spring-projects/spring-security/blob/main/core/src/main/resources/org/springframework/security/core/userdetails/jdbc/users.ddl	Gitee 镜像仓库：https://gitee.com/mirrors/spring-security/blob/main/core/src/main/resources/org/springframework/security/core/userdetails/jdbc/users.ddl
  * </p>
  *
  * @author xuxiaowei
- * @since 2022-04-04
+ * @since 2022-04-05
  */
 @Getter
 @Setter
@@ -21,12 +21,36 @@ public class Users implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    /**
+     * 用户名，唯一键：pk__users__username
+     */
     private String username;
 
+    /**
+     * 密码，不能为空
+     */
     @TableField("`password`")
     private String password;
 
+    /**
+     * 是否启用，不能为空
+     */
     private Boolean enabled;
+
+    /**
+     * 帐户未过期，不能为空
+     */
+    private Boolean accountNonExpired;
+
+    /**
+     * 凭证未过期，不能为空
+     */
+    private Boolean credentialsNonExpired;
+
+    /**
+     * 帐户未锁定，不能为空
+     */
+    private Boolean accountNonLocked;
 
     /**
      * 权限
