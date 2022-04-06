202390c1d2958ee9afd30080e075945ce14735bc
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-04-06 17:10:09 +0800

    Mybatis Plus 自动填充

---
+++ cloud-commons-parent/cloud-starter-mybatis/src/main/java/cloud/xuxiaowei/mybatis/configuration/MyBatisPlusMetaObjectHandler.java
@@ -0,0 +1,33 @@
+package cloud.xuxiaowei.mybatis.configuration;
+
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
+
+/**
+ * Mybatis Plus 自动填充
+ *
+ * @author 徐晓伟
+ * @since 0.0.1
+ */
+@Slf4j
+@Configuration
+public class MyBatisPlusMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+
+        strictInsertFill(metaObject, "createDate", LocalDateTime.class, LocalDateTime.now());
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+
+        strictInsertFill(metaObject, "updateDate", LocalDateTime.class, LocalDateTime.now());
+    }
+
+}
