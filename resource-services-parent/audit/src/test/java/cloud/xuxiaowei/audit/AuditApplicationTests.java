536cef61829450b1491e8c24a4b25fb985c18341
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-04-07 17:28:56 +0800

    add 审计服务

---
+++ resource-services-parent/audit/src/test/java/cloud/xuxiaowei/audit/AuditApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.xuxiaowei.audit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
