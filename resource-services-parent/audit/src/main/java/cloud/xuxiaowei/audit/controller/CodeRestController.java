376fb35c2ea80ae6c0ffd6fc5274bb573d233176
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-04-07 22:39:43 +0800

    add 授权码Code

---
+++ resource-services-parent/audit/src/main/java/cloud/xuxiaowei/audit/controller/CodeRestController.java
@@ -0,0 +1,32 @@
+package cloud.xuxiaowei.audit.controller;
+
+import cloud.xuxiaowei.utils.Response;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 授权码Code
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@RestController
+@RequestMapping("/code")
+public class CodeRestController {
+
+    /**
+     * 分页查询授权码
+     *
+     * @param request  请求
+     * @param response 响应
+     * @return 返回 分页查询结果
+     */
+    @RequestMapping("/page")
+    public Response<?> page(HttpServletRequest request, HttpServletResponse response) {
+        return Response.ok();
+    }
+
+}
