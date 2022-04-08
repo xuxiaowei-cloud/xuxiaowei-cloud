e0948831d427fc3c74a2ce9d41267a0afe8524e3
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-04-08 19:41:25 +0800

    init

---
+++ canal/src/main/java/cloud/xuxiaowei/canal/CanalApplication.java
@@ -0,0 +1,19 @@
+package cloud.xuxiaowei.canal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 程序执行入口
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@SpringBootApplication
+public class CanalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanalApplication.class, args);
+    }
+
+}
