a1f029ace7946c30871f66f5626a5bbdf4559503
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-04-08 11:27:34 +0800

    资源服务配置

---
+++ authorization-server/src/main/java/cloud/xuxiaowei/authorizationserver/configuration/ResourceServerConfigurerAdapterConfiguration.java
@@ -0,0 +1,52 @@
+package cloud.xuxiaowei.authorizationserver.configuration;
+
+import cloud.xuxiaowei.utils.Constant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+/**
+ * 资源服务配置
+ *
+ * @author xuxiaowei
+ * @see <a href="http://127.0.0.1:1301/sns/userinfo?access_token=">访问资源</a>
+ * @since 0.0.1
+ */
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfigurerAdapterConfiguration extends ResourceServerConfigurerAdapter {
+
+    private TokenStore tokenStore;
+
+    @Autowired
+    public void setTokenStore(TokenStore tokenStore) {
+        this.tokenStore = tokenStore;
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+
+        resources.tokenStore(tokenStore);
+
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        // 配置资源路径 /sns/** 需要的权限 scope
+        // http.antMatcher("/sns/**").authorizeRequests()
+        //        .antMatchers("/sns/userinfo").access("#oauth2.hasAnyScope('snsapi_base','snsapi_userinfo')");
+
+        // 端点放行
+        http.authorizeRequests().antMatchers("/" + Constant.ACTUATOR + "/**").permitAll();
+
+        // OAuth2 授权后可访问
+        http.antMatcher("/**").authorizeRequests()
+                .antMatchers("/**").access("#oauth2.isOAuth()");
+
+    }
+
+}
