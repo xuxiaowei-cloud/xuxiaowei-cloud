ee89abf91796b826674200a1d54b27ae857b3a21
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-04-09 23:45:10 +0800

    separate routes

---
+++ ui/src/router/home.js
@@ -0,0 +1,14 @@
+const home = [
+  {
+    path: '/home/homepage1',
+    name: 'homepage1',
+    component: import('@/views/home/homepage1.vue')
+  },
+  {
+    path: '/home/homepage2',
+    name: 'homepage2',
+    component: import('@/views/home/homepage2.vue')
+  }
+]
+
+export default home
