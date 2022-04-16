52892b7404944b4df2cb9d5d25fb7c9ae11ac35a
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-04-16 17:24:26 +0800

    fix package

--- ui/src/views/editor/ToastUiEditorView.vue
+++ ui/src/views/editor/ToastUiEditorView.vue
@@ -8,14 +8,18 @@ import Editor from '@toast-ui/editor'
 import '@toast-ui/editor/dist/toastui-editor.css' // Editor's Style
 
 onMounted(() => {
-  const editor = new Editor({
-    el: document.querySelector('#editor'),
-    height: '500px',
-    initialEditType: 'markdown',
-    previewStyle: 'vertical'
-  })
 
-  editor.getMarkdown()
+  const querySelectorEditor = document.querySelector('#editor')
+  if (querySelectorEditor instanceof HTMLElement) {
+    const editor = new Editor({
+      el: querySelectorEditor,
+      height: '500px',
+      initialEditType: 'markdown',
+      previewStyle: 'vertical'
+    })
+
+    editor.getMarkdown()
+  }
 })
 
 </script>
