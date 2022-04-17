e07b67c71e033b9b3f9a84d86d371f7398e55500
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-04-17 17:25:36 +0800

    http响应未授权代码将跳转到登录页面

--- ui/src/settings.ts
+++ ui/src/settings.ts
@@ -1,13 +1,31 @@
 /**
  * 设置
- * @type {{mutations: {}, state: {okCode: string}, actions: {}}}
  */
 const settings = {
   state: {
     /**
      * 正常代码
      */
-    okCode: '000000'
+    okCode: '000000',
+    /**
+     * 登录页面
+     */
+    loginPage: import.meta.env.VITE_APP_LOGIN_PAGE,
+    /**
+     * 登录请求代码（未授权代码）
+     */
+    loginRequiredCode: [
+      // 令牌异常
+      'T10000',
+      // 令牌未识别
+      'T10001',
+      // 令牌已过期
+      'T10002',
+      // 令牌缺失
+      'T10003',
+      // 令牌无效
+      'T10004'
+    ]
   },
   mutations: {},
   actions: {}
