fcaa2f968b0f5e59b643339211350cda9099f0d3
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-04-17 20:16:51 +0800

    判断是否拥有某项权限

--- ui/src/utils/authority.ts
+++ ui/src/utils/authority.ts
@@ -7,3 +7,16 @@ import store from '../store'
 export const hasAuthority = function (authority: string) {
   return store.getters.authoritiesList.indexOf(authority) !== -1
 }
+
+/**
+ * 判断是否拥有某项权限
+ * @param authoritys 权限
+ */
+export const hasAnyAuthority = function (authoritys: string[]) {
+  for (const authority in authoritys) {
+    if (store.getters.authoritiesList.indexOf(authority) !== -1) {
+      return true
+    }
+  }
+  return false
+}
