9f5c44d7ce70b9e83382afdfee114f8bd7001291
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-04-23 12:27:59 +0800

    rename

--- resource-services-parent/webservice/src/main/java/cloud/xuxiaowei/webservice/WebServiceApplication.java
+++ resource-services-parent/webservice/src/main/java/cloud/xuxiaowei/webservice/WebServiceApplication.java
@@ -10,10 +10,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 0.0.1
  */
 @SpringBootApplication
-public class WebserviceApplication {
+public class WebServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WebserviceApplication.class, args);
+        SpringApplication.run(WebServiceApplication.class, args);
     }
 
 }
