78c06cca0a00710436eaaccc22bf361f2a780724
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-04-23 16:23:01 +0800

    cxfServlet

--- resource-services-parent/webservice/src/main/java/cloud/xuxiaowei/webservice/point/CxfAuthenticationEntryPointImpl.java
+++ resource-services-parent/webservice/src/main/java/cloud/xuxiaowei/webservice/point/CxfAuthenticationEntryPointImpl.java
@@ -1,7 +1,12 @@
 package cloud.xuxiaowei.webservice.point;
 
 import cloud.xuxiaowei.utils.RequestUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.cxf.transport.servlet.CXFServlet;
+import org.apache.cxf.transport.servlet.ServletController;
 import org.apache.http.entity.ContentType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -15,16 +20,26 @@ import java.io.IOException;
  * CXF 身份验证入口点
  *
  * @author xuxiaowei
+ * @see ServletController
  * @since 0.0.1
  */
+@Slf4j
 @Component
 public class CxfAuthenticationEntryPointImpl implements AuthenticationEntryPoint {
+
+    private ServletRegistrationBean<CXFServlet> cxfServlet;
+
+    @Autowired
+    public void setCxfServlet(ServletRegistrationBean<CXFServlet> cxfServlet) {
+        this.cxfServlet = cxfServlet;
+    }
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
 
         String inputStream = RequestUtils.getInputStream(request);
-        System.out.println(inputStream);
+        log.info(inputStream);
+
 
         response.setContentType(ContentType.APPLICATION_SOAP_XML.toString());
 
