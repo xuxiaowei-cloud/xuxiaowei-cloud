12c33d85757c73cdc8ae8ae786edd9a8c7d7cf9c
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-04-25 10:31:45 +0800

    路径、IP匹配 测试类

--- gateway/src/test/java/cloud/xuxiaowei/gateway/RequestMatcherTests.java
+++ gateway/src/test/java/cloud/xuxiaowei/gateway/RequestMatcherTests.java
@@ -1,4 +1,4 @@
-package cloud.xuxiaowei.passport;
+package cloud.xuxiaowei.gateway;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.mvc.condition.PatternsRequestCondition;
 import java.util.List;
 
 /**
- * 路径匹配 测试类
+ * 路径、IP匹配 测试类
  *
  * @author xuxiaowei
  * @since 0.0.1
@@ -16,11 +16,23 @@ import java.util.List;
 @Slf4j
 class RequestMatcherTests {
 
+    /**
+     * /24：254：256
+     * /25：126：128
+     * /26：62：64
+     * /27：30：32
+     * /28：14：16
+     * /29：6：8
+     * /30：2：4
+     */
     @Test
     void ipAddressMatcher() {
-        IpAddressMatcher ipAddressMatcher = new IpAddressMatcher("192.168.5.1/24");
-        boolean matches = ipAddressMatcher.matches("192.168.5.2");
-        System.out.println(matches);
+        IpAddressMatcher ipAddressMatcher = new IpAddressMatcher("192.168.5.4/30");
+        for (int i = 0; i < 256; i++) {
+            String ip = "192.168.5." + i;
+            boolean matches = ipAddressMatcher.matches(ip);
+            System.out.println(ip + "\t" + matches);
+        }
     }
 
     @Test
