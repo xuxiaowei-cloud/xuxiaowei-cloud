d7c1984383d2f43f88a0936fa52f725eab4a411c
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-04-25 11:17:37 +0800

    Clean

--- gateway/src/test/java/cloud/xuxiaowei/gateway/RequestMatcherTests.java
+++ gateway/src/test/java/cloud/xuxiaowei/gateway/RequestMatcherTests.java
@@ -2,15 +2,21 @@ package cloud.xuxiaowei.gateway;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.springframework.security.web.util.matcher.IpAddressMatcher;
-import org.springframework.web.servlet.mvc.condition.PatternsRequestCondition;
-
-import java.util.List;
+import org.springframework.security.web.util.matcher.*;
+import org.springframework.web.reactive.result.condition.RequestMethodsRequestCondition;
+import org.springframework.web.reactive.result.method.RequestMappingInfo;
 
 /**
  * 路径、IP匹配 测试类
  *
  * @author xuxiaowei
+ * @see RequestMatcher
+ * @see ELRequestMatcher
+ * @see RequestHeaderRequestMatcher
+ * @see MediaTypeRequestMatcher
+ * @see RegexRequestMatcher
+ * @see RequestMethodsRequestCondition
+ * @see RequestMappingInfo
  * @since 0.0.1
  */
 @Slf4j
@@ -33,26 +39,6 @@ class RequestMatcherTests {
             boolean matches = ipAddressMatcher.matches(ip);
             System.out.println(ip + "\t" + matches);
         }
-    }
-
-    @Test
-    void patternsRequestCondition() {
-        PatternsRequestCondition patternsRequestCondition = new PatternsRequestCondition("/login/**", "/login2/**");
-
-        List<String> login = patternsRequestCondition.getMatchingPatterns("/login");
-        log.info(String.valueOf(login.size()));
-
-        List<String> loginCode = patternsRequestCondition.getMatchingPatterns("/login/code");
-        log.info(String.valueOf(loginCode.size()));
-
-        List<String> loginCodeAbc = patternsRequestCondition.getMatchingPatterns("/login/code/abc");
-        log.info(String.valueOf(loginCodeAbc.size()));
-
-        List<String> login2CodeAbc = patternsRequestCondition.getMatchingPatterns("/login2/code/abc");
-        log.info(String.valueOf(login2CodeAbc.size()));
-
-        List<String> login3CodeAbc = patternsRequestCondition.getMatchingPatterns("/login3/code/abc");
-        log.info(String.valueOf(login3CodeAbc.size()));
     }
 
 }
