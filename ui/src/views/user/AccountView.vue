09d02c591ef7f248a50ed3726a1042e9c9f52558
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-04-28 17:32:59 +0800

    账户绑定（暂存）

--- ui/src/views/user/AccountView.vue
+++ ui/src/views/user/AccountView.vue
@@ -1,10 +1,18 @@
 <template>
-  <el-container>
-    账户绑定
-  </el-container>
+  <el-tabs v-model="activeName" class="demo-tabs">
+    <el-tab-pane label="手机号" name="phone">
+      手机号
+    </el-tab-pane>
+    <el-tab-pane label="邮箱" name="mail">
+      邮箱
+    </el-tab-pane>
+  </el-tabs>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+
+const activeName = ref('phone')
 
 </script>
 
