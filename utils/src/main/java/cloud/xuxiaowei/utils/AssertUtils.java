af9cba25b3b622c633c83ca53bb2ab60835bcd0b
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-04-28 17:04:49 +0800

    最大长度断言

--- utils/src/main/java/cloud/xuxiaowei/utils/AssertUtils.java
+++ utils/src/main/java/cloud/xuxiaowei/utils/AssertUtils.java
@@ -76,4 +76,68 @@ public class AssertUtils {
         }
     }
 
+    /**
+     * 最大长度断言
+     *
+     * @param list    数据
+     * @param max     最大长度
+     * @param message 消息
+     */
+    public static void max(List<?> list, int max, String message) {
+        if (list != null) {
+            if (list.size() > max) {
+                throw new CloudRuntimeException(message);
+            }
+        }
+    }
+
+    /**
+     * 最大长度断言
+     *
+     * @param list    数据
+     * @param max     最大长度
+     * @param message 消息
+     */
+    public static void maxNonNull(List<?> list, int max, String message) {
+        if (list == null) {
+            throw new CloudRuntimeException(message);
+        }
+
+        if (list.size() > max) {
+            throw new CloudRuntimeException(message);
+        }
+    }
+
+    /**
+     * 最大长度断言
+     *
+     * @param string  数据
+     * @param max     最大长度
+     * @param message 消息
+     */
+    public static void max(String string, int max, String message) {
+        if (string != null) {
+            if (string.length() > max) {
+                throw new CloudRuntimeException(message);
+            }
+        }
+    }
+
+    /**
+     * 最大长度断言
+     *
+     * @param string  数据
+     * @param max     最大长度
+     * @param message 消息
+     */
+    public static void maxNonNull(String string, int max, String message) {
+        if (string == null) {
+            throw new CloudRuntimeException(message);
+        }
+
+        if (string.length() > max) {
+            throw new CloudRuntimeException(message);
+        }
+    }
+
 }
