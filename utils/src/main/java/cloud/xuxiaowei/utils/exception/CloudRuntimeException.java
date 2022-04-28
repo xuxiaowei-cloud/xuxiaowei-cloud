8993c65a3582a2dcae7744b775549ba658f90c04
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-04-28 16:36:23 +0800

    微服务 运行时异常父类
    目前仅提供给断言使用

---
+++ utils/src/main/java/cloud/xuxiaowei/utils/exception/CloudRuntimeException.java
@@ -0,0 +1,96 @@
+package cloud.xuxiaowei.utils.exception;
+
+import cloud.xuxiaowei.utils.CodeEnums;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Setter;
+
+/**
+ * 微服务 运行时异常父类
+ * <p>
+ * 目前仅提供给断言使用
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class CloudRuntimeException extends RuntimeException {
+
+    /**
+     * 错误码
+     */
+    @Setter(AccessLevel.NONE)
+    private String code;
+
+    /**
+     * 中文描述
+     */
+    @Setter(AccessLevel.NONE)
+    private String msg;
+
+    /**
+     * 错误字段
+     * <p>
+     * 存在多个时，使用英文逗号隔开
+     */
+    @Setter(AccessLevel.NONE)
+    private String field;
+
+    /**
+     * 说明
+     */
+    @Setter(AccessLevel.NONE)
+    private String explain;
+
+    public CloudRuntimeException() {
+        this.code = CodeEnums.ERROR.code;
+        this.msg = CodeEnums.ERROR.msg;
+    }
+
+    public CloudRuntimeException(String msg) {
+        this.code = CodeEnums.ERROR.code;
+        this.msg = msg;
+    }
+
+    public CloudRuntimeException(String code, String msg) {
+        super(msg);
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public CloudRuntimeException(String code, String msg, Throwable cause) {
+        super(msg, cause);
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public CloudRuntimeException(String code, String msg, String field, Throwable cause) {
+        super(msg, cause);
+        this.code = code;
+        this.msg = msg;
+        this.field = field;
+    }
+
+    public CloudRuntimeException(String code, String msg, String field, String explain, Throwable cause) {
+        super(msg, cause);
+        this.code = code;
+        this.msg = msg;
+        this.field = field;
+        this.explain = explain;
+    }
+
+    public CloudRuntimeException(Throwable cause) {
+        super(CodeEnums.ERROR.msg, cause);
+        this.code = CodeEnums.ERROR.code;
+        this.msg = CodeEnums.ERROR.msg;
+    }
+
+    public CloudRuntimeException(String msg, Throwable cause) {
+        super(msg, cause);
+        this.code = CodeEnums.ERROR.code;
+        this.msg = msg;
+    }
+
+}
