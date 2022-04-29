5987cf85e4966bda297f5a1e91cac8952784fc8a
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-04-29 14:28:50 +0800

    response error

--- passport-ui/src/utils/request.js
+++ passport-ui/src/utils/request.js
@@ -1,4 +1,5 @@
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
+import { ElMessage } from 'element-plus'
 
 // create an axios instance
 const service = axios.create({
@@ -23,6 +24,32 @@ service.interceptors.response.use(
     return response
   },
   error => {
+    if (error instanceof AxiosError) {
+      if (error.message === 'timeout of ' + error.config.timeout + 'ms exceeded') {
+        ElMessage({
+          message: '请求超时，请重试',
+          // 显示时间，单位为毫秒。设为 0 则不会自动关闭，类型：number，默认值：3000
+          duration: 3000,
+          type: 'error'
+        })
+      } else {
+        ElMessage({
+          message: error.message,
+          // 显示时间，单位为毫秒。设为 0 则不会自动关闭，类型：number，默认值：3000
+          duration: 3000,
+          type: 'error'
+        })
+      }
+    } else if (error.message === 'Network Error') {
+      ElMessage({
+        message: '网络错误，请稍后再试',
+        // 显示时间，单位为毫秒。设为 0 则不会自动关闭，类型：number，默认值：3000
+        duration: 3000,
+        type: 'error'
+      })
+    } else {
+      console.log(error)
+    }
     return error
   }
 )
