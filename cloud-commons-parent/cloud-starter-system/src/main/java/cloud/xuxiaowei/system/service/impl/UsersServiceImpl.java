192106b5d587a90780e88645bc6ed6d83a7dbaf3
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-04-30 17:32:28 +0800

    分页查询用户

--- cloud-commons-parent/cloud-starter-system/src/main/java/cloud/xuxiaowei/system/service/impl/UsersServiceImpl.java
+++ cloud-commons-parent/cloud-starter-system/src/main/java/cloud/xuxiaowei/system/service/impl/UsersServiceImpl.java
@@ -1,12 +1,20 @@
 package cloud.xuxiaowei.system.service.impl;
 
+import cloud.xuxiaowei.system.bo.ManageUsersPageBo;
 import cloud.xuxiaowei.system.entity.Users;
 import cloud.xuxiaowei.system.mapper.UsersMapper;
 import cloud.xuxiaowei.system.service.IUsersService;
 import cloud.xuxiaowei.system.vo.UsersVo;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -51,6 +59,51 @@ public class UsersServiceImpl extends ServiceImpl<UsersMapper, Users> implements
         BeanUtils.copyProperties(users, usersVo);
 
         return usersVo;
+    }
+
+    /**
+     * 分页查询用户
+     *
+     * @param manageUsersPageBo 管理用户分页参数
+     * @return 返回 分页查询结果
+     */
+    @Override
+    public IPage<UsersVo> pageByManageUsers(ManageUsersPageBo manageUsersPageBo) {
+        QueryWrapper<Users> queryWrapper = new QueryWrapper<>();
+        Long current = manageUsersPageBo.getCurrent();
+        Long size = manageUsersPageBo.getSize();
+
+        Long usersId = manageUsersPageBo.getUsersId();
+        String username = manageUsersPageBo.getUsername();
+        String nickname = manageUsersPageBo.getNickname();
+
+        if (usersId != null) {
+            queryWrapper.eq("users_id", usersId);
+        }
+        if (StringUtils.hasText(username)) {
+            queryWrapper.eq("username", username);
+        }
+        if (StringUtils.hasText(nickname)) {
+            queryWrapper.eq("nickname", nickname);
+        }
+
+        IPage<Users> page = new Page<>(current == null ? 1 : current, size == null ? 10 : size);
+        page(page, queryWrapper);
+
+        Page<UsersVo> usersVoPage = new Page<>();
+        BeanUtils.copyProperties(page, usersVoPage);
+
+        List<UsersVo> usersVoList = new ArrayList<>();
+        usersVoPage.setRecords(usersVoList);
+
+        List<Users> records = page.getRecords();
+        for (Users users : records) {
+            UsersVo usersVo = new UsersVo();
+            BeanUtils.copyProperties(users, usersVo);
+            usersVoList.add(usersVo);
+        }
+
+        return usersVoPage;
     }
 
 }
