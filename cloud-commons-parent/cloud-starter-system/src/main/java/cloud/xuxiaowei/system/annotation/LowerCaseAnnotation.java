978af4346a118329248c21893a52644a2280dfc5
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-05-09 14:56:55 +0800

    是否包含小写字母/大写字母/数字/特殊符号 验证

---
+++ cloud-commons-parent/cloud-starter-system/src/main/java/cloud/xuxiaowei/system/annotation/LowerCaseAnnotation.java
@@ -0,0 +1,27 @@
+package cloud.xuxiaowei.system.annotation;
+
+import cloud.xuxiaowei.system.validation.LowerCaseValidation;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+/**
+ * 是否包含小写字母 验证
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@Documented
+@Constraint(validatedBy = {LowerCaseValidation.class})
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LowerCaseAnnotation {
+
+    String message() default "内容必须包含小写字母";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
--- utils/src/main/java/cloud/xuxiaowei/utils/Constant.java
+++ utils/src/main/java/cloud/xuxiaowei/utils/Constant.java
@@ -1,6 +1,8 @@
 package cloud.xuxiaowei.utils;
 
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * 常量
@@ -11,6 +13,35 @@ import java.io.Serializable;
 public class Constant implements Serializable {
 
     private static final long serialVersionUID = 1L;
+
+    /**
+     * 数字
+     */
+    public static final List<String> NUMBER_LIST = Arrays.asList("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+
+    /**
+     * 小写字母
+     */
+    public static final List<String> LOWER_CASE_LIST = Arrays.asList(
+            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+            "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+            "u", "v", "w", "x", "y", "z");
+
+    /**
+     * 大写字母
+     */
+    public static final List<String> UPPER_CASE_LIST = Arrays.asList(
+            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+            "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+            "U", "V", "W", "X", "Y", "Z");
+
+    /**
+     * 符号
+     */
+    public static final List<String> SYMBOL = Arrays.asList(
+            "!", "@", "#", "$", "%", "^", "&", "*", "(", ")",
+            "-", "_", "=", "+", "[", "{", "]", "}", ";", ":",
+            "'", "\"", ",", "<", ".", ">", "/", "?");
 
     /**
      * 端点
