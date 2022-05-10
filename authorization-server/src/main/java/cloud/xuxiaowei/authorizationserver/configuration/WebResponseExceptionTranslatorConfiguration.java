d9839a70873fc2261bae061ec98f20a387b8e007
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-05-10 16:10:41 +0800

    授权类型不支持

--- authorization-server/src/main/java/cloud/xuxiaowei/authorizationserver/configuration/WebResponseExceptionTranslatorConfiguration.java
+++ authorization-server/src/main/java/cloud/xuxiaowei/authorizationserver/configuration/WebResponseExceptionTranslatorConfiguration.java
@@ -160,6 +160,13 @@ public class WebResponseExceptionTranslatorConfiguration implements WebResponseE
 
                 log.error("授权码异常：", oauth2Exception);
 
+            } else if(oauth2Exception instanceof UnsupportedGrantTypeException) {
+
+                oauth2Exception.addAdditionalInformation(Response.CODE, CodeEnums.C10007.code);
+                oauth2Exception.addAdditionalInformation(Response.MSG, CodeEnums.C10007.msg);
+
+                log.error("不支持的授权类型异常：", oauth2Exception);
+
             } else {
                 String message = oauth2Exception.getMessage();
                 if (CODE_ENUMS_T10001_MESSAGE.equals(message)) {
