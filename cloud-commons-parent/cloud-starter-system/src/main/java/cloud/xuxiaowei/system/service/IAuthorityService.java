e0eae456fccd5692bc741c818f1c02464f2bab81
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-05-23 10:17:56 +0800

    查询、分页查询 权限与权限说明表

--- cloud-commons-parent/cloud-starter-system/src/main/java/cloud/xuxiaowei/system/service/IAuthorityService.java
+++ cloud-commons-parent/cloud-starter-system/src/main/java/cloud/xuxiaowei/system/service/IAuthorityService.java
@@ -1,7 +1,12 @@
 package cloud.xuxiaowei.system.service;
 
+import cloud.xuxiaowei.system.bo.AuthorityBo;
+import cloud.xuxiaowei.system.bo.AuthorityPageBo;
 import cloud.xuxiaowei.system.entity.Authority;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
 
 /**
  * <p>
@@ -12,5 +17,21 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2022-05-17
  */
 public interface IAuthorityService extends IService<Authority> {
+
+    /**
+     * 查询 权限与权限说明表
+     *
+     * @param authorityBo 权限与权限说明参数
+     * @return 返回 查询结果
+     */
+    List<Authority> listByAuthorityBo(AuthorityBo authorityBo);
+
+    /**
+     * 分页查询 权限与权限说明表
+     *
+     * @param authorityPageBo 权限与权限说明分页参数
+     * @return 返回 查询结果
+     */
+    IPage<Authority> pageByAuthorityPageBo(AuthorityPageBo authorityPageBo);
 
 }
--- resource-services-parent/user/src/main/java/cloud/xuxiaowei/user/controller/AuthorityController.java
+++ resource-services-parent/user/src/main/java/cloud/xuxiaowei/user/controller/AuthorityController.java
@@ -1,10 +1,15 @@
 package cloud.xuxiaowei.user.controller;
 
+import cloud.xuxiaowei.system.annotation.ControllerAnnotation;
+import cloud.xuxiaowei.system.bo.AuthorityBo;
+import cloud.xuxiaowei.system.bo.AuthorityPageBo;
 import cloud.xuxiaowei.system.entity.Authority;
 import cloud.xuxiaowei.system.service.IAuthorityService;
 import cloud.xuxiaowei.utils.Response;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,15 +39,35 @@ public class AuthorityController {
     /**
      * 获取 权限与权限说明 字典
      *
-     * @param request  请求
-     * @param response 响应
+     * @param request     请求
+     * @param response    响应
+     * @param authorityBo 权限与权限说明参数
      * @return 返回 结果
      */
+    @ControllerAnnotation(description = "获取 权限与权限说明 字典")
     @RequestMapping("/list")
     @PreAuthorize("hasAuthority('manage_user_authority') or #oauth2.hasScope('manage_user_authority')")
-    public Response<?> list(HttpServletRequest request, HttpServletResponse response) {
+    public Response<?> list(HttpServletRequest request, HttpServletResponse response, @RequestBody AuthorityBo authorityBo) {
 
-        List<Authority> authorityList = authorityService.list();
+        List<Authority> authorityList = authorityService.listByAuthorityBo(authorityBo);
+
+        return Response.ok(authorityList);
+    }
+
+    /**
+     * 获取 权限与权限说明 分页字典
+     *
+     * @param request         请求
+     * @param response        响应
+     * @param authorityPageBo 权限与权限说明分页参数
+     * @return 返回 结果
+     */
+    @ControllerAnnotation(description = "获取 权限与权限说明 分页字典")
+    @RequestMapping("/page")
+    @PreAuthorize("hasAuthority('manage_user_authority') or #oauth2.hasScope('manage_user_authority')")
+    public Response<?> page(HttpServletRequest request, HttpServletResponse response, @RequestBody AuthorityPageBo authorityPageBo) {
+
+        IPage<Authority> authorityList = authorityService.pageByAuthorityPageBo(authorityPageBo);
 
         return Response.ok(authorityList);
     }
