65da76e9b11f4b610ced3951f7631c6fdfa49607
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-05-23 21:33:29 +0800

    客户表

---
+++ cloud-commons-parent/cloud-starter-system/src/main/java/cloud/xuxiaowei/system/entity/OauthClientDetails.java
@@ -0,0 +1,55 @@
+package cloud.xuxiaowei.system.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 原表结构：https://github.com/spring-projects/spring-security-oauth/blob/main/spring-security-oauth2/src/test/resources/schema.sql	GitCode 镜像仓库：https://gitcode.net/mirrors/spring-projects/spring-security-oauth/-/blob/master/spring-security-oauth2/src/test/resources/schema.sql	Gitee 镜像仓库：https://gitee.com/mirrors/spring-security-oauth/blob/master/spring-security-oauth2/src/test/resources/schema.sql
+ * </p>
+ *
+ * @author xuxiaowei
+ * @since 2022-05-23
+ */
+@Getter
+@Setter
+@TableName("oauth_client_details")
+public class OauthClientDetails implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "oauth_client_details_id", type = IdType.AUTO)
+    private Long oauthClientDetailsId;
+
+    /**
+     * 唯一键：uk__oauth_client_details__client_id
+     */
+    private String clientId;
+
+    private String resourceIds;
+
+    private String clientSecret;
+
+    private String scope;
+
+    private String authorizedGrantTypes;
+
+    private String webServerRedirectUri;
+
+    private String authorities;
+
+    private Integer accessTokenValidity;
+
+    private Integer refreshTokenValidity;
+
+    private String additionalInformation;
+
+    private String autoapprove;
+
+
+}
