b0ee65a662945ae1c65375d91297960189fd6dca
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-06-07 09:01:03 +0800

    腾讯云对象存储

--- cos.sh
+++ cos.sh
@@ -1,3 +1,15 @@
+echo "cos:" > .cos.yaml
+echo "  base:" >> .cos.yaml
+echo "    secretid: $secretid" >> .cos.yaml
+echo "    secretkey: $secretkey" >> .cos.yaml
+echo "    sessiontoken: \"\"" >> .cos.yaml
+echo "    protocol: https" >> .cos.yaml
+echo "  buckets:" >> .cos.yaml
+echo "    - name: $bucketname_appid" >> .cos.yaml
+echo "      alias: $bucketname_appid" >> .cos.yaml
+echo "      region: \"\"" >> .cos.yaml
+echo "      endpoint: $cos_endpoint" >> .cos.yaml
+
 echo "根据要储存文件名的特点查找文件（排除.m2文件夹）"
 for file in $(find . -path ./.m2 -prune -o -name *-*-*.pom -o -name *-*-*.jar -o -name *-*-*.*.asc); do
   if [ './.m2' == $file ]; then
