5612e3009b03c38d54814b0e397c3c2a0588d5c6
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-06-07 17:42:38 +0800

    add 根据 用户名 删除Token、根据 客户ID 删除Token

--- ui/src/api/authorization-server.ts
+++ ui/src/api/authorization-server.ts
@@ -87,3 +87,23 @@ export const updateById = function (data: any) {
     return response.data
   })
 }
+
+/**
+ * 根据 用户名 删除Token
+ * @param usernames 用户名
+ */
+export const removeByUsernames = function (usernames: string[]) {
+  return request.post('/authorization-server/oauth-token/removeByUsernames', usernames).then(response => {
+    return response.data
+  })
+}
+
+/**
+ * 根据 客户ID 删除Token
+ * @param clientIds 客户ID
+ */
+export const removeByClientIds = function (clientIds: string[]) {
+  return request.post('/authorization-server/oauth-token/removeByClientIds', clientIds).then(response => {
+    return response.data
+  })
+}
