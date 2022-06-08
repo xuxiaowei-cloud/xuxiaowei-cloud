d9ecd3dd994d82d6eb674ada7c20f221b8ac8f72
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-06-08 17:14:22 +0800

    服务（组件，系统模块）：在日志中添加用户标识

--- utils/src/main/java/cloud/xuxiaowei/utils/SecurityUtils.java
+++ utils/src/main/java/cloud/xuxiaowei/utils/SecurityUtils.java
@@ -1,6 +1,8 @@
 package cloud.xuxiaowei.utils;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
 
 import java.util.Map;
@@ -14,6 +16,17 @@ import java.util.Map;
 public class SecurityUtils {
 
     /**
+     * 获取 用户名
+     *
+     * @return 返回 用户名
+     */
+    public static String getUserName() {
+        SecurityContext context = SecurityContextHolder.getContext();
+        Authentication authentication = context.getAuthentication();
+        return getUserName(authentication);
+    }
+
+    /**
      * 根据 验证 获取 用户名
      *
      * @param authentication 验证
@@ -21,6 +34,11 @@ public class SecurityUtils {
      */
     public static String getUserName(Authentication authentication) {
         if (authentication != null) {
+            String name = authentication.getName();
+            if (name != null) {
+                return name;
+            }
+
             Object principal = authentication.getPrincipal();
             if (principal instanceof Jwt) {
                 Jwt jwt = (Jwt) principal;
