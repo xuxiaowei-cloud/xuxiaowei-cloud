34efbf6eeedde086529cf7882e16eb7b339cdbb7
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-07-10 14:51:52 +0800

    :white_check_mark: 同步 xuxiaowei-cloud-next 项目测试用例

---
+++ passport/src/test/java/cloud/xuxiaowei/passport/TokenSettingsTests.java
@@ -0,0 +1,50 @@
+package cloud.xuxiaowei.passport;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.jackson2.SecurityJackson2Modules;
+import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.TokenSettings;
+import org.springframework.security.oauth2.server.authorization.jackson2.OAuth2AuthorizationServerJackson2Module;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 客户Token配置 测试类
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@Slf4j
+class TokenSettingsTests {
+
+	@Test
+	void settings() throws JsonProcessingException {
+		TokenSettings.Builder tokenSettingsBuilder = TokenSettings.builder();
+
+		// 授权Token时间
+		tokenSettingsBuilder.accessTokenTimeToLive(Duration.ofHours(12));
+		// 刷新Token时间
+		tokenSettingsBuilder.refreshTokenTimeToLive(Duration.ofDays(30));
+
+		TokenSettings tokenSettings = tokenSettingsBuilder.build();
+		Map<String, Object> settings = tokenSettings.getSettings();
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		ClassLoader classLoader = JdbcRegisteredClientRepository.class.getClassLoader();
+		List<Module> securityModules = SecurityJackson2Modules.getModules(classLoader);
+		objectMapper.registerModules(securityModules);
+		objectMapper.registerModule(new OAuth2AuthorizationServerJackson2Module());
+
+		ObjectWriter objectWriter = objectMapper.writerWithDefaultPrettyPrinter();
+		String s = objectWriter.writeValueAsString(settings);
+		log.info("\n{}", s);
+	}
+
+}
