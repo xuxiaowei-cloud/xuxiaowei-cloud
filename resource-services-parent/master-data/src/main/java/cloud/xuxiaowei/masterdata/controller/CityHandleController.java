462b6dd7ec5f2693f0f800ec3e6338a34dee073a
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-08-20 13:09:10 +0800

    :construction: 分页查询城市

--- resource-services-parent/master-data/src/main/java/cloud/xuxiaowei/masterdata/controller/CityHandleController.java
+++ resource-services-parent/master-data/src/main/java/cloud/xuxiaowei/masterdata/controller/CityHandleController.java
@@ -1,7 +1,19 @@
 package cloud.xuxiaowei.masterdata.controller;
 
+import cloud.xuxiaowei.masterdata.bo.CityHandlePageBo;
+import cloud.xuxiaowei.masterdata.entity.CityHandle;
+import cloud.xuxiaowei.masterdata.service.ICityHandleService;
+import cloud.xuxiaowei.utils.Response;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 /**
  * <p>
@@ -12,7 +24,29 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2022-08-20
  */
 @RestController
-@RequestMapping("//city-handle")
+@RequestMapping("/city-handle")
 public class CityHandleController {
+
+	private ICityHandleService cityHandleService;
+
+	@Autowired
+	public void setCityHandleService(ICityHandleService cityHandleService) {
+		this.cityHandleService = cityHandleService;
+	}
+
+	/**
+	 * 分页查询城市
+	 * @param request 请求
+	 * @param response 响应
+	 * @param cityHandlePageBo 分页参数
+	 * @return 返回 查询结果
+	 */
+	@PreAuthorize("hasAuthority('region_read')")
+	@RequestMapping("/page")
+	public Response<?> page(HttpServletRequest request, HttpServletResponse response,
+			@Valid @RequestBody CityHandlePageBo cityHandlePageBo) {
+		IPage<CityHandle> page = cityHandleService.pageByCityHandlePageBo(cityHandlePageBo);
+		return Response.ok(page);
+	}
 
 }
--- resource-services-parent/master-data/src/main/java/cloud/xuxiaowei/masterdata/service/impl/CityHandleServiceImpl.java
+++ resource-services-parent/master-data/src/main/java/cloud/xuxiaowei/masterdata/service/impl/CityHandleServiceImpl.java
@@ -1,10 +1,15 @@
 package cloud.xuxiaowei.masterdata.service.impl;
 
+import cloud.xuxiaowei.masterdata.bo.CityHandlePageBo;
 import cloud.xuxiaowei.masterdata.entity.CityHandle;
 import cloud.xuxiaowei.masterdata.mapper.CityHandleMapper;
 import cloud.xuxiaowei.masterdata.service.ICityHandleService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 /**
  * <p>
@@ -16,5 +21,34 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class CityHandleServiceImpl extends ServiceImpl<CityHandleMapper, CityHandle> implements ICityHandleService {
+
+	/**
+	 * 分页查询城市
+	 * @param cityHandlePageBo 分页参数
+	 * @return 返回 查询结果
+	 */
+	@Override
+	public IPage<CityHandle> pageByCityHandlePageBo(CityHandlePageBo cityHandlePageBo) {
+		QueryWrapper<CityHandle> queryWrapper = new QueryWrapper<>();
+		Long current = cityHandlePageBo.getCurrent();
+		Long size = cityHandlePageBo.getSize();
+		Integer provinceCode = cityHandlePageBo.getProvinceCode();
+		Integer cityCode = cityHandlePageBo.getCityCode();
+		String cityName = cityHandlePageBo.getCityName();
+
+		if (provinceCode != null) {
+			queryWrapper.eq("province_code", provinceCode);
+		}
+		if (cityCode != null) {
+			queryWrapper.eq("city_code", cityCode);
+		}
+		if (StringUtils.hasText(cityName)) {
+			queryWrapper.eq("city_name", cityName);
+		}
+
+		IPage<CityHandle> page = new Page<>(current == null ? 1 : current, size == null ? 10 : size);
+
+		return page(page, queryWrapper);
+	}
 
 }
