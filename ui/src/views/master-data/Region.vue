8ad1668648d9801d872f14d91ab87c97c0de6d45
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-08-20 16:10:37 +0800

    :sparkles: 区域：精确查询

--- ui/src/views/master-data/Region.vue
+++ ui/src/views/master-data/Region.vue
@@ -1,40 +1,82 @@
 <template>
+  <el-header style="text-align: center">
+    <el-switch v-model="type" inactive-text="模糊查询" active-text="精确查询"/>
+  </el-header>
 
-  <el-select clearable filterable v-model="province" placeholder="请选择省份" size="large">
-    <el-option v-for="item in provinceOptions" :key="item.provinceCode" :value="item.provinceCode"
-               :label="item.provinceName"/>
-  </el-select>
+  <div v-if="type">
+    <el-select clearable filterable v-model="province" placeholder="请选择省份" size="large">
+      <el-option v-for="item in provinceOptions" :key="item.provinceCode" :value="item.provinceCode"
+                 :label="item.provinceName"/>
+    </el-select>
 
-  <el-select clearable filterable v-model="city" placeholder="请选择城市" size="large"
-             :disabled="province === undefined || province === ''">
-    <el-option v-for="item in cityOptions" :key="item.cityCode" :value="item.cityCode" :label="item.cityName"/>
-  </el-select>
+    <el-select clearable filterable v-model="city" placeholder="请选择城市" size="large"
+               :disabled="province === undefined || province === ''">
+      <el-option v-for="item in cityOptions" :key="item.cityCode" :value="item.cityCode" :label="item.cityName"/>
+    </el-select>
 
-  <el-select clearable filterable v-model="county" placeholder="请选择区/县" size="large"
-             :disabled="city === undefined || city === ''">
-    <el-option v-for="item in countyOptions" :key="item.countyCode" :value="item.countyCode" :label="item.countyName"/>
-  </el-select>
+    <el-select clearable filterable v-model="county" placeholder="请选择区/县" size="large"
+               :disabled="city === undefined || city === ''">
+      <el-option v-for="item in countyOptions" :key="item.countyCode" :value="item.countyCode"
+                 :label="item.countyName"/>
+    </el-select>
 
-  <el-select clearable filterable v-model="town" placeholder="请选择镇" size="large"
-             :disabled="county === undefined || county === ''">
-    <el-option v-for="item in townOptions" :key="item.townCode" :value="item.townCode" :label="item.townName"/>
-  </el-select>
+    <el-select clearable filterable v-model="town" placeholder="请选择镇" size="large"
+               :disabled="county === undefined || county === ''">
+      <el-option v-for="item in townOptions" :key="item.townCode" :value="item.townCode" :label="item.townName"/>
+    </el-select>
 
-  <el-select clearable filterable v-model="village" placeholder="请选择居委会" size="large"
-             :disabled="town === undefined || town === ''">
-    <el-option v-for="item in villageOptions" :key="item.villageCode" :value="item.villageCode"
-               :label="item.villageName"/>
-  </el-select>
+    <el-select clearable filterable v-model="village" placeholder="请选择居委会" size="large"
+               :disabled="town === undefined || town === ''">
+      <el-option v-for="item in villageOptions" :key="item.villageCode" :value="item.villageCode"
+                 :label="item.villageName"/>
+    </el-select>
 
+    <el-button @click="cloudSearch" style="height: 38px">搜索</el-button>
+  </div>
+  <el-container>
+    <el-table stripe :data="tableData" v-loading="loading" height="460" @cell-dblclick="rowDblClick">
+      <el-table-column prop="year" label="year" width="80"/>
+
+      <el-table-column prop="provinceCode" label="provinceCode" width="120"/>
+      <el-table-column prop="provinceName" label="provinceName" width="160"/>
+
+      <el-table-column prop="cityCode" label="cityCode" width="90"/>
+      <el-table-column prop="cityName" label="cityName" width="160"/>
+
+      <el-table-column prop="countyCode" label="countyCode" width="110"/>
+      <el-table-column prop="countyName" label="countyName" width="160"/>
+
+      <el-table-column prop="townCode" label="townCode" width="110"/>
+      <el-table-column prop="townName" label="townName" width="160"/>
+
+      <el-table-column prop="villageCode" label="villageCode" width="120"/>
+      <el-table-column prop="villageName" label="villageName" width="180"/>
+      <el-table-column prop="villageTypeCode" label="villageTypeCode" width="140"/>
+    </el-table>
+  </el-container>
+  <el-container>
+    <el-pagination layout="prev, pager, next, total" @current-change="currentChange" :total="param.total"/>
+  </el-container>
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from 'vue'
+import { reactive, ref, watch } from 'vue'
+import { ElMessage } from 'element-plus/es'
+import useClipboard from 'vue-clipboard3'
+
 import { page as provincePage } from '../../api/master-data/province-handle'
 import { page as cityPage } from '../../api/master-data/city-handle'
 import { page as countyPage } from '../../api/master-data/county-handle'
 import { page as townPage } from '../../api/master-data/town-handle'
 import { page as villagePage } from '../../api/master-data/village-handle'
+import { page as regionPage } from '../../api/master-data/region'
+import settings from '../../settings'
+
+// 复制
+const { toClipboard } = useClipboard()
+
+// 查询类型
+const type = ref<boolean>(true)
 
 // 省
 const province = ref()
@@ -145,6 +187,67 @@ watch(() => town.value, (newValue, oldValue) => {
     })
   }
 })
+
+const param = reactive({
+  current: 1,
+  size: 10,
+  total: 0,
+  provinceCode: null,
+  cityCode: null,
+  countyCode: null,
+  townCode: null,
+  villageCode: null
+})
+
+const tableData = ref([])
+
+// 加载
+const loading = ref(true)
+
+// 搜索
+const cloudSearch = () => {
+  loading.value = true
+  param.provinceCode = province.value
+  param.cityCode = city.value
+  param.countyCode = county.value
+  param.townCode = town.value
+  param.villageCode = village.value
+  regionPage(param).then(response => {
+    console.log(response)
+    if (response.code === settings.okCode) {
+      const data = response.data
+      tableData.value = data.records
+      param.total = data.total
+      loading.value = false
+    } else {
+      ElMessage.error(response.msg)
+    }
+  })
+}
+
+// 改变时触发
+const currentChange = (e: number) => {
+  param.current = e
+  cloudSearch()
+}
+
+// 默认搜索
+cloudSearch()
+
+// 当某个单元格被双击击时会触发该事件
+const rowDblClick = async (row: any, column: any, cell: any, event: any) => {
+  const columnValue = row[column.property]
+  console.log(columnValue)
+  try {
+    await toClipboard(columnValue)
+    ElMessage({
+      message: '已复制到剪贴板。',
+      type: 'success'
+    })
+  } catch (e) {
+    console.error(e)
+  }
+}
 
 </script>
 
