ca4f6595130e784b27be40157e3b575c2c1da720
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-08-25 23:06:19 +0800

    :white_check_mark: 依赖去重

--- utils/src/test/java/cloud/xuxiaowei/utils/DependenciesTests.java
+++ utils/src/test/java/cloud/xuxiaowei/utils/DependenciesTests.java
@@ -6,6 +6,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * 处理依赖分析
@@ -58,6 +60,46 @@ class DependenciesTests {
 			e.printStackTrace();
 		}
 		System.out.println(stringBuilder);
+	}
+
+	@Test
+	void set() {
+		File file = new File("D:/tree.txt");
+		Set<String> set = new HashSet<>();
+		try (// 创建一个新FileReader，给定从中读取文件 。
+				FileReader fileReader = new FileReader(file);
+
+				// 创建一个使用默认大小输入缓冲器的缓冲字符输入流。
+				BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+
+			String readLine;
+			// 读取文本行。 的线被认为是由一个进料线中的任何一个被终止（“\n”），回车（“\r”），或回车立即由换行遵循。
+			while ((readLine = bufferedReader.readLine()) != null) {
+
+				if (readLine.contains("[INFO] cloud.xuxiaowei")) {
+
+				}
+				else if (readLine.contains("[INFO] +- ")) {
+
+				}
+				else if ("[INFO] ".equals(readLine)) {
+
+				}
+				else {
+					continue;
+				}
+				if (readLine.contains("cloud.xuxiaowei")) {
+					continue;
+				}
+				set.add(readLine);
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		for (String line : set) {
+			System.out.println(line);
+		}
 	}
 
 }
