69ed717c6e75e4a4541ed26c1eecdad1d528367b
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-10-22 22:44:24 +0800

    :art: spring-javaformat

--- cloud-commons-parent/cloud-starter-system/src/main/java/cloud/xuxiaowei/system/service/IWeiBoWebsiteUsersService.java
+++ cloud-commons-parent/cloud-starter-system/src/main/java/cloud/xuxiaowei/system/service/IWeiBoWebsiteUsersService.java
@@ -13,27 +13,27 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IWeiBoWebsiteUsersService extends IService<WeiBoWebsiteUsers> {
 
-    /**
-     * 根据 微博开放平台-网站用户ID、用户唯一标识 查询用户
-     * @param appid 微博开放平台-网站用户ID
-     * @param id 用户唯一标识
-     * @return 返回 用户
-     */
-    WeiBoWebsiteUsers getByAppidAndId(String appid, String id);
+	/**
+	 * 根据 微博开放平台-网站用户ID、用户唯一标识 查询用户
+	 * @param appid 微博开放平台-网站用户ID
+	 * @param id 用户唯一标识
+	 * @return 返回 用户
+	 */
+	WeiBoWebsiteUsers getByAppidAndId(String appid, String id);
 
-    /**
-     * 社交绑定
-     * @param usersId 用户主键
-     * @param appid 微博开放平台-网站用户ID
-     * @param id 用户唯一标识
-     */
-    void binding(long usersId, String appid, long id);
+	/**
+	 * 社交绑定
+	 * @param usersId 用户主键
+	 * @param appid 微博开放平台-网站用户ID
+	 * @param id 用户唯一标识
+	 */
+	void binding(long usersId, String appid, long id);
 
-    /**
-     * 社交解绑
-     * @param usersId 用户主键
-     * @return 返回 解绑结果
-     */
-    boolean unbinding(Long usersId);
+	/**
+	 * 社交解绑
+	 * @param usersId 用户主键
+	 * @return 返回 解绑结果
+	 */
+	boolean unbinding(Long usersId);
 
 }
