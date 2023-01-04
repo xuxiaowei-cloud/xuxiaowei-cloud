b29627bc6fca90334d607569f326564fd310cc31
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-01-04 17:33:00 +0800

    :sparkles: 增加序列化

--- cloud-commons-parent/cloud-starter-session-redis/src/main/java/cloud/xuxiaowei/session/configuration/SessionRedisSerializerConfiguration.java
+++ cloud-commons-parent/cloud-starter-session-redis/src/main/java/cloud/xuxiaowei/session/configuration/SessionRedisSerializerConfiguration.java
@@ -8,8 +8,10 @@ import org.springframework.context.annotation.Import;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.security.jackson2.CoreJackson2Module;
+import org.springframework.security.jackson2.SecurityJackson2Modules;
 import org.springframework.security.web.jackson2.WebJackson2Module;
 import org.springframework.security.web.jackson2.WebServletJackson2Module;
+import org.springframework.security.web.server.jackson2.WebServerJackson2Module;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.data.redis.config.annotation.web.http.RedisHttpSessionConfiguration;
 
@@ -44,7 +46,23 @@ public class SessionRedisSerializerConfiguration {
 
 		// Web、Security 序列化与反序列化
 		// https://github.com/spring-projects/spring-security/issues/4370
-		objectMapper.registerModules(new WebServletJackson2Module(), new WebJackson2Module(), new CoreJackson2Module());
+		SecurityJackson2Modules.enableDefaultTyping(objectMapper);
+
+		// 以下使用显性引入
+		// 用于支持：Cookie、SavedCookie、DefaultSavedRequest、WebAuthenticationDetails 反序列化
+		objectMapper.registerModule(new WebServletJackson2Module());
+		// 用于支持：org.springframework.security.web.csrf.DefaultCsrfToken、PreAuthenticatedAuthenticationToken
+		// 反序列化
+		objectMapper.registerModule(new WebJackson2Module());
+		// 用于支持：AnonymousAuthenticationToken、RememberMeAuthenticationToken、SimpleGrantedAuthority、Set、List、Map、
+		// User、UsernamePasswordAuthenticationToken、BadCredentialsException 反序列化
+		objectMapper.registerModule(new CoreJackson2Module());
+		// 用于支持：org.springframework.security.web.server.csrf.DefaultCsrfToken 反序列化
+		objectMapper.registerModule(new WebServerJackson2Module());
+
+		// objectMapper.registerModule(new CasJackson2Module());
+		// objectMapper.registerModule(new OAuth2ClientJackson2Module());
+		// objectMapper.registerModule(new Saml2Jackson2Module());
 
 		jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
 
