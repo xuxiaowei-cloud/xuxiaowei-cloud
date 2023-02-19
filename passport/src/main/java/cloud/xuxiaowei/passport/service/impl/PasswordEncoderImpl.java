2f0ae4b766bad650f5a72cc2d02e3ff60acb9dde
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-02-19 23:11:08 +0800

    :bug: 客户密码认证

--- passport/src/main/java/cloud/xuxiaowei/passport/service/impl/PasswordEncoderImpl.java
+++ passport/src/main/java/cloud/xuxiaowei/passport/service/impl/PasswordEncoderImpl.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames;
+import org.springframework.security.oauth2.server.authorization.authentication.*;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
@@ -19,8 +20,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 /**
  * 密码编辑器
@@ -32,6 +31,23 @@ import java.util.Set;
 @Slf4j
 @Component
 public class PasswordEncoderImpl implements PasswordEncoder {
+
+	// @formatter:off
+	private static final List<String> THIRD_PARTY_GRANTTYPE = Arrays.asList(
+			OAuth2AlipayMiniProgramAuthenticationToken.ALIPAY_MINIPROGRAM.getValue(),
+			OAuth2AlipayOplatformWebsiteAuthenticationToken.ALIPAY_OPLATFORM_WEBSITE.getValue(),
+			OAuth2DingtalkAuthenticationToken.DINGTALK.getValue(),
+			OAuth2GiteeAuthenticationToken.GITEE.getValue(),
+			OAuth2GitHubAuthenticationToken.GITHUB.getValue(),
+			OAuth2GitLabAuthenticationToken.GITLAB.getValue(),
+			OAuth2QQMiniProgramAuthenticationToken.QQ_MINIPROGRAM.getValue(),
+			OAuth2QQWebsiteAuthenticationToken.QQ_WEBSITE.getValue(),
+			OAuth2WeChatMiniProgramAuthenticationToken.WECHAT_MINIPROGRAM.getValue(),
+			OAuth2WeChatOffiaccountAuthenticationToken.WECHAT_OFFIACCOUNT.getValue(),
+			OAuth2WeChatOplatformWebsiteAuthenticationToken.WECHAT_OPLATFORM_WEBSITE.getValue(),
+			OAuth2WeChatWorkWebsiteAuthenticationToken.WECHAT_WORK_WEBSITE.getValue(),
+			OAuth2WeiBoWebsiteAuthenticationToken.WEIBO_WEBSITE.getValue());
+	// @formatter:on
 
 	private HttpServletRequest request;
 
@@ -68,12 +84,9 @@ public class PasswordEncoderImpl implements PasswordEncoder {
 
 		boolean matches;
 
-		Map<String, String[]> parameterMap = request.getParameterMap();
-		int size = parameterMap.size();
-		Set<String> keySet = parameterMap.keySet();
-		List<String> list = Arrays.asList(OAuth2ParameterNames.CODE, OAuth2ParameterNames.CLIENT_ID,
-				OAuth2ParameterNames.CLIENT_SECRET, OAuth2ParameterNames.REDIRECT_URI, OAuth2ParameterNames.GRANT_TYPE);
-		if (size == list.size() && keySet.containsAll(list)) {
+		String grantType = request.getParameter(OAuth2ParameterNames.GRANT_TYPE);
+		String requestUri = request.getRequestURI();
+		if ("/oauth2/token".equals(requestUri) && THIRD_PARTY_GRANTTYPE.contains(grantType)) {
 			log.info("客户密码认证");
 			matches = passwordEncoder.matches(rawPassword, encodedPassword);
 		}
