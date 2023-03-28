17cddca55c78512990910c312b55e22c511c96d9
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-03-28 23:02:45 +0800

    :sparkles: 登录服务使用Redis缓存减少查询OAuth2.1的表

--- cloud-commons-parent/cloud-starter-oauth2/src/main/java/cloud/xuxiaowei/oauth2/configuration/OAuth2AuthorizationServiceJwtAuthenticationConverter.java
+++ cloud-commons-parent/cloud-starter-oauth2/src/main/java/cloud/xuxiaowei/oauth2/configuration/OAuth2AuthorizationServiceJwtAuthenticationConverter.java
@@ -1,11 +1,11 @@
 // package cloud.xuxiaowei.oauth2.configuration;
 //
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.context.annotation.Configuration;
 // import org.springframework.core.convert.converter.Converter;
 // import org.springframework.lang.NonNull;
 // import org.springframework.security.authentication.AbstractAuthenticationToken;
-// import org.springframework.security.core.GrantedAuthority;
 // import org.springframework.security.oauth2.jwt.Jwt;
-// import org.springframework.security.oauth2.jwt.JwtClaimNames;
 // import org.springframework.security.oauth2.server.authorization.OAuth2Authorization;
 // import
 // org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
@@ -13,13 +13,6 @@
 // import org.springframework.security.oauth2.server.resource.InvalidBearerTokenException;
 // import
 // org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-// import
-// org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-// import
-// org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
-// import org.springframework.util.Assert;
-//
-// import java.util.Collection;
 //
 /// **
 // * JWT {@link OAuth2AuthorizationService}
@@ -28,26 +21,29 @@
 // * @since 6.0.0
 // * @see JwtAuthenticationConverter
 // * @see OAuth2AuthorizationService
+// * @see JwtAuthenticationConverterConfiguration 与此类配合使用
 // */
+// @Configuration
 // public class OAuth2AuthorizationServiceJwtAuthenticationConverter
 // implements Converter<Jwt, AbstractAuthenticationToken> {
 //
-// private final OAuth2AuthorizationService authorizationService;
+// private OAuth2AuthorizationService authorizationService;
 //
-// public OAuth2AuthorizationServiceJwtAuthenticationConverter(
-// @NonNull OAuth2AuthorizationService authorizationService) {
-// Assert.notNull(authorizationService, "authorizationService cannot be null");
+// private JwtAuthenticationConverter jwtAuthenticationConverter;
+//
+// @Autowired
+// public void setAuthorizationService(OAuth2AuthorizationService authorizationService) {
 // this.authorizationService = authorizationService;
 // }
 //
-// private Converter<Jwt, Collection<GrantedAuthority>> jwtGrantedAuthoritiesConverter =
-// new JwtGrantedAuthoritiesConverter();
-//
-// private String principalClaimName = JwtClaimNames.SUB;
+// @Autowired
+// public void setJwtAuthenticationConverter(JwtAuthenticationConverter
+// jwtAuthenticationConverter) {
+// this.jwtAuthenticationConverter = jwtAuthenticationConverter;
+// }
 //
 // @Override
 // public final AbstractAuthenticationToken convert(@NonNull Jwt jwt) {
-//
 // String tokenValue = jwt.getTokenValue();
 // OAuth2Authorization authorization = authorizationService.findByToken(tokenValue,
 // OAuth2TokenType.ACCESS_TOKEN);
@@ -55,24 +51,7 @@
 // throw new InvalidBearerTokenException(tokenValue + " not found in
 // OAuth2AuthorizationService");
 // }
-//
-// Collection<GrantedAuthority> authorities =
-// this.jwtGrantedAuthoritiesConverter.convert(jwt);
-//
-// String principalClaimValue = jwt.getClaimAsString(this.principalClaimName);
-// return new JwtAuthenticationToken(jwt, authorities, principalClaimValue);
-// }
-//
-// public void setJwtGrantedAuthoritiesConverter(
-// Converter<Jwt, Collection<GrantedAuthority>> jwtGrantedAuthoritiesConverter) {
-// Assert.notNull(jwtGrantedAuthoritiesConverter, "jwtGrantedAuthoritiesConverter cannot
-// be null");
-// this.jwtGrantedAuthoritiesConverter = jwtGrantedAuthoritiesConverter;
-// }
-//
-// public void setPrincipalClaimName(String principalClaimName) {
-// Assert.hasText(principalClaimName, "principalClaimName cannot be empty");
-// this.principalClaimName = principalClaimName;
+// return jwtAuthenticationConverter.convert(jwt);
 // }
 //
 // }
