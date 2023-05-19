9221c746e8800649ec0ab339d8cf418bcae7ed13
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-05-19 13:08:11 +0800

    :white_check_mark: GitHub API createSecret

--- utils/src/test/java/cloud/xuxiaowei/utils/GItHubAPITests.java
+++ utils/src/test/java/cloud/xuxiaowei/utils/GItHubAPITests.java
@@ -8,6 +8,8 @@ import org.kohsuke.github.GitHub;
 import org.kohsuke.github.GitHubBuilder;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * GitHub API
@@ -20,6 +22,8 @@ class GItHubAPITests {
 
 	private static final String OAUTH_TOKEN = "";
 
+	private static final String JIHULAB_PRIVATE_TOKEN = "";
+
 	@Test
 	void getPublicKey() throws IOException {
 		GitHub github = new GitHubBuilder().withOAuthToken(OAUTH_TOKEN).build();
@@ -29,6 +33,31 @@ class GItHubAPITests {
 		GHRepositoryPublicKey publicKey = repo.getPublicKey();
 
 		log.info(String.valueOf(publicKey));
+	}
+
+	@Test
+	void createSecret() throws IOException {
+		GitHub github = new GitHubBuilder().withOAuthToken(OAUTH_TOKEN).build();
+
+		String targetSuper = "xuxiaowei-com-cn";
+		List<String> targetList = Arrays.asList("spring-boot-starter", "spring-boot-starter-alipay-miniprogram",
+				"spring-boot-starter-alipay-oplatform", "spring-boot-starter-dingtalk",
+				"spring-boot-starter-feishu-webpage", "spring-boot-starter-gitee", "spring-boot-starter-github",
+				"spring-boot-starter-gitlab", "spring-boot-starter-idempotent", "spring-boot-starter-qq-connect",
+				"spring-boot-starter-qq-miniprogram", "spring-boot-starter-redis",
+				"spring-boot-starter-wechat-miniprogram", "spring-boot-starter-wechat-offiaccount",
+				"spring-boot-starter-wechat-oplatform", "spring-boot-starter-wechat-work", "spring-boot-starter-weibo",
+				"spring-security-oauth2-authorization-server-redis");
+
+		String secretName = "JIHULAB_PRIVATE_TOKEN";
+
+		for (String target : targetList) {
+			GHRepository repo = github.getRepository(targetSuper + "/" + target);
+			GHRepositoryPublicKey publicKey = repo.getPublicKey();
+			String keyId = publicKey.getKeyId();
+			repo.createSecret(secretName, JIHULAB_PRIVATE_TOKEN, keyId);
+		}
+
 	}
 
 }
